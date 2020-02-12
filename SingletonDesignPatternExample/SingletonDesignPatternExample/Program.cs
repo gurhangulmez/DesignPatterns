3fec234c5a02a0008d34550024ac4259143acfb4
Gurhan Gulmez <gurhangulmez@gmail.com>
2020-02-12 21:31:47 +0000

    First Commit

---
+++ SingletonDesignPatternExample/SingletonDesignPatternExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SingletonDesignPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a thread-safety SingletonDesignPattern Example!");
+
+            //*** USE only SingletonClass.GetInstance ***/
+            //No need to assign it to another variable when using
+
+
+            //LET'S CHECK!
+
+            // The client code.
+            SingletonClass s1 = SingletonClass.GetInstance;
+            SingletonClass s2 = SingletonClass.GetInstance;
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
---
+++ SingletonDesignPatternExample/SingletonDesignPatternExample/SingletonClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDesignPatternExample
+{
+    public sealed class SingletonClass
+    {
+        private static readonly object lockObj = new object();
+        private static SingletonClass instance;
+
+        private SingletonClass()
+        {
+        }
+
+        public static SingletonClass GetInstance
+        {
+            get
+            { 
+                lock(lockObj)
+                {
+                    if (instance == null)
+                    {
+                        instance = new SingletonClass();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void WriteMyName()
+        {
+            Console.WriteLine("HELLO! I am thread-safety Singleton Class!");
+        }
+    }
+}
