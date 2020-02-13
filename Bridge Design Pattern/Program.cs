9479f98f2a8d776243f4019dbc1675e6b2ba996b
Gurhan Gulmez <gurhangulmez@gmail.com>
2020-02-13 21:48:54 +0000

    Bridge DP

---
+++ Bridge Design Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bridge_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager cManager = new CustomerManager();
+            cManager.messageSenderBase = new EMailSender();
+
+            cManager.UpdateCustomer();           
+        }
+    }
+}
